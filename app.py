ade2ba0e373725ca5e54da98155b0329e7e628f4
Matheus Rocha Salvador Da Silva <mrmatheus34@gmail.com>
2021-04-16 19:01:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -22,9 +22,7 @@ sales_by_cust = df.groupby("Customer type")["Unit price"].sum()
 sales_by_gender = df.groupby("Gender")["Unit price"].sum()
  
  
-def dashboard():
- 
-   fig = make_subplots(
+fig = make_subplots(
 rows = 3, cols = 4,
 specs = [
 [{"type": "indicator"}, {"type": "indicator", "colspan": 2}, None, {"type": "indicator"}],
@@ -39,21 +37,19 @@ subplot_titles =
 ]
 )
  
-   # indicators
-   fig.add_trace(go.Indicator(mode = "number", title = "Total Gross Income", value = total_gross), row = 1, col = 1)
-   fig.add_trace(go.Indicator(mode = "number", title = "Total Sales", value = total_sales), row = 1, col = 2)   
-   fig.add_trace(go.Indicator(mode = "number", title = "Total Units", value = total_units), row = 1, col = 4)
+# indicators
+fig.add_trace(go.Indicator(mode = "number", title = "Total Gross Income", value = total_gross), row = 1, col = 1)
+fig.add_trace(go.Indicator(mode = "number", title = "Total Sales", value = total_sales), row = 1, col = 2)   
+fig.add_trace(go.Indicator(mode = "number", title = "Total Units", value = total_units), row = 1, col = 4)
  
-   # time series
-   fig.add_trace(go.Scatter(x = sales_by_day.index, y = sales_by_day.values, marker_color = "springgreen", name = "Sales By Day of Month"), row = 2, col = 1)
+# time series
+fig.add_trace(go.Scatter(x = sales_by_day.index, y = sales_by_day.values, marker_color = "springgreen", name = "Sales By Day of Month"), row = 2, col = 1)
  
-   # bottom
-   fig.add_trace(go.Bar(x = sales_by_city.index, y = sales_by_city.values, marker_color = "mediumspringgreen", name = "Sales By City"), row = 3, col = 1)
-   fig.add_trace(go.Bar(x = sales_by_line.index, y = sales_by_line.values, marker_color = "mediumspringgreen", name = "Sales By Product Line"), row = 3, col = 2)
-   fig.add_trace(go.Bar(x = sales_by_cust.index, y = sales_by_cust.values, marker_color = "mediumspringgreen", name = "Sales By Customer Type"), row = 3, col = 3)
-   fig.add_trace(go.Pie(values = sales_by_gender.values, labels = sales_by_gender.index, hole = .6, marker_colors = ["lime", "lightgreen"], name = "Sales By Gender"), row = 3, col = 4)
+# bottom
+fig.add_trace(go.Bar(x = sales_by_city.index, y = sales_by_city.values, marker_color = "mediumspringgreen", name = "Sales By City"), row = 3, col = 1)
+fig.add_trace(go.Bar(x = sales_by_line.index, y = sales_by_line.values, marker_color = "mediumspringgreen", name = "Sales By Product Line"), row = 3, col = 2)
+fig.add_trace(go.Bar(x = sales_by_cust.index, y = sales_by_cust.values, marker_color = "mediumspringgreen", name = "Sales By Customer Type"), row = 3, col = 3)
+fig.add_trace(go.Pie(values = sales_by_gender.values, labels = sales_by_gender.index, hole = .6, marker_colors = ["lime", "lightgreen"], name = "Sales By Gender"), row = 3, col = 4)
  
-   fig.update_layout(template = "plotly_dark", title = "Sales Dashboard", width = 800, height = 600, legend_orientation = "v")
-   fig.show()
- 
-dashboard()
+fig.update_layout(template = "plotly_dark", title = "Sales Dashboard", width = 800, height = 600, legend_orientation = "v")
+fig.show()
